97ec4837073bceef7c97e818f2101c120064c06f
cuculuis <luisangel96@gmail.com>
2023-05-09 03:21:42 -0300

    2-ECMAScriptAvanzado: Arreglo de método de validación.

--- 2-ECMAScriptAvanzado/manager.js
+++ 2-ECMAScriptAvanzado/manager.js
@@ -3,8 +3,18 @@ class ProductManager {
     constructor() {
         this.products = [];
         this.idProducts = 1
+        this.error = undefined
     }
 
+    validateProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            return this.error = `campos incompletos`
+        } else {
+            const found = this.products.find(product => product.code === code)
+            if (found) {return this.error = `El code: ${code} ya existe`}
+            else {return this.error = undefined}
+        }
+    }
 
 
     addProduct(object) {
@@ -13,14 +23,16 @@ class ProductManager {
             object.code = codigo
         }
 
-        const existingProduct = this.products.find((p) => p.code === this.products.code);
-        if (existingProduct) {
-            throw new Error("El código de producto ya existe.");
+        this.validateProduct(object.title, object.description, object.price, object.thumbnail, object.code, object.stock)
+        if (this.error === undefined) {
+            object.id = this.idProducts;
+            this.products.push(object);
+            this.idProducts++;
+        }
+        else {
+            console.log(this.error);
         }
 
-        object.id = this.idProducts;
-        this.products.push(object);
-        this.idProducts++;
     }
 
     getProducts() {
@@ -31,7 +43,7 @@ class ProductManager {
         const product = this.products.find((product) => product.id === productId);
     
         if (!product) {
-            throw new Error("Producto no encontrado.");
+            return "Producto no encontrado.";
         }
 
         return product;
@@ -40,29 +52,51 @@ class ProductManager {
 
 const productmanager = new ProductManager()
 
+console.log('/-------------------------------------/');
 console.log('/--1) Primer llamado de getProducts--/');
+console.log('/-----------------------------------/');
 console.log(productmanager.getProducts());
 
-productmanager.addProduct({title: "Mango", descripcion: "Fruta tropical", price: 200, thumbnail: "mango.jpg", code: "123456", stock: 50})
+productmanager.addProduct({title: "Mango", description: "Fruta tropical", price: 200, thumbnail: "mango.jpg", code: "123456", stock: 50})
 
-productmanager.addProduct({title: "Pera", descripcion: "Fruta Pomacea", price: 100, thumbnail: "pera.jpg", code: "123456", stock: 30})
+console.log('/---------------------------------------------------------/');
+console.log('/--No se agrega Pera porque tiene un code igual a Mango--/');
+console.log('/-------------------------------------------------------/');
+productmanager.addProduct({title: "Pera", description: "Fruta Pomacea", price: 100, thumbnail: "pera.jpg", code: "123456", stock: 30})
 
-productmanager.addProduct({title: "Limon", descripcion: "Fruta cítrica", price: 50, thumbnail: "pera.jpg", stock: 35})
+productmanager.addProduct({title: "Limon", description: "Fruta cítrica", price: 50, thumbnail: "pera.jpg", stock: 35})
 
-productmanager.addProduct({title: "Zanahoria", descripcion: "Verdura", price: 80, thumbnail: "zanahoria.jpg", stock: 60})
+productmanager.addProduct({title: "Zanahoria", description: "Verdura", price: 80, thumbnail: "zanahoria.jpg", stock: 60})
 
+console.log('/------------------------------------------/');
 console.log('/--2) Llamado de getProducts 2do intento--/');
+console.log('/----------------------------------------/');
 console.log(productmanager.getProducts());
 
+console.log('/--------------------------------/');
 console.log('/--3) Llamado de getProductById--/');
+console.log('/--------------------------------/');
+
+console.log('/--------------------------------------/');
 console.log('/--a) Llamado de getProductById id 1--/');
+console.log('/------------------------------------/');
 console.log(productmanager.getProductById(1));
+console.log('/-----------------------------------------------------------/');
+console.log('/--3) Llamado de getProductById id 5 (Producto no existe)--/');
+console.log('/-----------------------------------------------------------/');
+console.log(productmanager.getProductById(5));
+console.log('/--------------------------------------/');
 console.log('/--3) Llamado de getProductById id 3--/');
+console.log('/------------------------------------/');
 console.log(productmanager.getProductById(3));
+console.log('/--------------------------------------/');
 console.log('/--3) Llamado de getProductById id 2--/');
+console.log('/------------------------------------/');
 console.log(productmanager.getProductById(2));
 
-console.log('/----------Fin----------/');
+console.log('/--------------------------------/');
+console.log('/----------Fin------------------/');
+console.log('/------------------------------/');
 
 
 
